be7cad29b7523d8637269e32a1285f2cb85d4721
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-10-24 19:32:33 +0900

    新規登録完了画面のスタイリング調整。 Dennou-001

--- resources/views/Auth/register_success.blade.php
+++ resources/views/Auth/register_success.blade.php
@@ -1,6 +1,8 @@
 @include('header')
 <section class="register-success-wrapper">
     <div class="container">
-        <h2>新規登録が完了しました。</h2>
+        <div class="register-success-inner">
+            <h2>おめでとうございます。新規登録が完了しました。</h2>
+        </div>
     </div>
 </section>
