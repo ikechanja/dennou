654ba2dba7ccf73ad9b7ca531dcc6a42a1c6a231
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-10-24 17:35:33 +0900

    新規登録画面の完成 Dennou-001

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -5,8 +5,17 @@
             <div>
                 <h2>新規登録</h2>
                 <p>最初のステップは簡単です。新規登録画面へようこそ。</p>
-                <p>メールアドレス</p>
-                <input class="sign-input" type="email" name="email">
+                <form method="POST">
+                    <p>メールアドレス</p>
+                    <input class="sign-input" type="email" name="email">
+                    <p>ペンネーム</p>
+                    <input class="sign-input" type="text" name="username">
+                    <p>パスワード</p>
+                    <input class="sign-input" type="password" name="password">
+                    <p>パスワード（確認）</p>
+                    <input class="sign-input" type="password" name="password_conf">
+                    <input class="sign-submit" type="submit" value="新規登録">
+                </form>
             </div>
         </div>
     </div>
