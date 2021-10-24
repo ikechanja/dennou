341289cb73d423e92e8904376265c533b58abcd5
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-10-24 20:10:20 +0900

    ログイン画面の雛形を作成。 Dennou-001

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,1 +1,10 @@
 @include('header')
+<section class="login-wrapper">
+    <div>
+        <h2>ログイン</h2>
+        <p>お帰りなさい。さぁ、小説の世界へダイブしましょう。</p>
+        <form method="POST">
+
+        </form>
+    </div>
+</section>
