e953065fc90c620e5eebb644b02e08d7dcb5f09f
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-10-24 16:53:44 +0900

    about文言の追加 Dennou-001

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,4 +1,16 @@
 @include('header')
 <section class="top-wrapper">
-
+</section>
+<section class="about-wrapper">
+    <div class="container">
+        <div class="inner">
+            <div class="about-inner">
+                <h1>新しくなった電脳小説都市</h1>
+                <p>電脳小説都市が新しくなりました。
+                    電脳小説都市は大規模化に耐えられる環境構築を考えました。
+                    前バージョンより頑丈なWeb小説投稿サイトをお楽しみください。
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
